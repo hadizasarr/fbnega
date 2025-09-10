935626a66e9ad8537514dac488590c7cc3e304ff
Muthu0356 <muthulakshmi.roses@gmail.com>
2025-09-10 18:18:22 -0400

    added year over year plots

---
+++ src/plot_year_over_year.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+def plot_all_counties():
+    # Load CSV
+    file_path = "../data/total_monthly_distributions.csv"
+    output_dir = "../data/Year-Over-Year Plots"
+    os.makedirs(output_dir, exist_ok=True)
+
+    # delete old plots in output folder
+    for file in os.listdir(output_dir):
+        if file.endswith(".png"):
+            os.remove(os.path.join(output_dir, file))
+
+    try:
+        df = pd.read_csv(file_path)
+    except Exception as e:
+        print(f"Error loading file: {e}")
+        return
+    
+    df.columns = df.columns.str.strip()
+    df["Month"] = pd.to_datetime(df["Month"], format="%Y-%m")
+
+    df["Fiscal Year"] = df["Source"].str.extract(r'(FY \d{4})')
+    df["Fiscal Month"] = df["Month"].dt.month
+
+    # Get unique counties
+    unique_counties = df["County"].dropna().unique()
+
+    # we want to plot from Jul of one year to Jun of the next year
+    month_pos = {7:0,8:1,9:2,10:3,11:4,12:5,1:6,2:7,3:8,4:9,5:10,6:11}
+    month_labels = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+
+    for county in unique_counties:
+        county_df = df[df["County"] == county].copy()
+
+        plt.figure(figsize=(10,6))
+
+        for fy in ['FY 2023', 'FY 2024', 'FY 2025']:
+            fy_data = county_df[county_df['Fiscal Year'] == fy]
+            x = fy_data["Fiscal Month"].map(month_pos)
+            # here, each element of x is paired with each element of fy_data["Weight"],
+            # internally, i ranges from 0 to length of fy_data - 1, and
+            # this plots a point at x[i], fy_data["Weight"][i]
+            plt.plot(x, fy_data['Weight'], marker='o', label=fy)
+
+        plt.xticks(ticks=list(month_pos.values()), labels=month_labels)
+        plt.xlabel("Month")
+        plt.ylabel("Food distribution weight")
+        plt.title(f"Year-Over-Year Food Distribution by Month - {county}")
+        plt.legend(title = "Fiscal Year")
+        plt.grid(True)
+        plt.tight_layout()
+
+        # Save plot to file
+        filename = county.replace(" ", "_").upper() + "_plot.png"
+        filepath = os.path.join(output_dir, filename)
+        plt.savefig(filepath)
+        plt.close()
+
+        print(f"Saved plot: {filepath}")
+
+plot_all_counties()
